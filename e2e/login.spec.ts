f41d55313bb218d4e62806d5d17402c67a1e2d64
桜藤紫苑 <sakurafujishion@sakurafujishionnoMacBook-Air.local>
2026-04-06 22:10:33 +0900

    test: split login and inventory detail test

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -4,11 +4,19 @@ import { InventoryPage } from '../pages/InventoryPage';
 
 test('standard user can log in and see products', async ({ page }) => {
   const loginPage = new LoginPage(page);
-  const inventoryPage = new InventoryPage(page);
 
   await loginPage.goto();
   await loginPage.login('standard_user', 'secret_sauce');
   await loginPage.expectInventoryPage();
+});
+
+test('user can open backpack detail', async ({page})=>{
+  const loginPage = new LoginPage(page);
+
+  await loginPage.goto();
+  await loginPage.login('standard_user', 'secret_sauce');
+
+  const inventoryPage = new InventoryPage(page);
 
   await inventoryPage.openBackpackDetail();
   await inventoryPage.expectBackpackDetailPage();
